7ea9600136b4a355741722228496d7d924b9a99b
FrankBakkerNl <frank.bakker@avivasolutions.nl>
2019-12-14 23:28:08 +0100

    Day12:P1

---
+++ AdventOfCode2019/Day12.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using static System.Math;
+
+namespace AdventOfCode2019
+{
+    public class Day12
+    {
+        public static int GetAnswer1(string[] input) => GetEnergyAfterSteps(input, 1000);
+
+        public static int GetEnergyAfterSteps(string[] input, int steps)
+        {
+            var state = LoadInitialState(input);
+            for (var i = 0; i < steps; i++)
+            {
+                state = Step(state).ToList();
+            }
+            return state.Sum(Energy);
+        }
+
+        public static IList<(Vector p, Vector)> LoadInitialState(string[] input)
+        {
+            var positions = Parse(input);
+            return positions.Select(p => (p, new Vector())).ToList();
+        }
+
+        public static int Energy((Vector p, Vector v) t) => Energy(t.p) * Energy(t.v);
+
+        public static int Energy(Vector v) => Abs(v.x) + Abs(v.y) + Abs(v.z);
+
+        public static IEnumerable<(Vector p, Vector v)> Step(IList<(Vector p, Vector velocity)> state) =>
+            from moon in state
+            let newVelocity = state.Aggregate(moon.velocity, (velocity, other) => velocity.Add(GetForce(moon.p, other.p)))
+            select (moon.p.Add(newVelocity), newVelocity);
+
+        public static Vector GetForce(Vector current, Vector other) =>
+            new Vector(GetForce(current.x, other.x), 
+                       GetForce(current.y, other.y), 
+                       GetForce(current.z, other.z));
+
+        private static int GetForce(int x, int y ) => x > y ? -1 :
+                                                      x < y ?  1 : 0;
+
+        public static IEnumerable<Vector> Parse(string[] input) => input.Select(Parse).ToArray();
+
+        public static Vector Parse(string input)
+        {
+            var match = Regex.Match(input, "x= *(?<x>-?[0-9]+), y= *(?<y>-?[0-9]+), z= *(?<z>-?[0-9]+)");
+            return new Vector(int.Parse(match.Groups["x"].Value),
+                              int.Parse(match.Groups["y"].Value),
+                              int.Parse(match.Groups["z"].Value));
+        }
+    }
+
+    public struct Vector
+    {
+        public int x, y, z; 
+
+        public Vector(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public Vector Add(Vector other) => new Vector(x + other.x, y + other.y, z + other.z);
+        public override string ToString() => $"({x},{y},{z})";
+    }
+}
---
+++ AdventOfCodeTest2019Test/Day12Test.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AdventOfCode2019;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCodeTest2019Test
+{
+    public class Day12Test
+    {
+        [Fact]
+        public void TestParse()
+        {
+            var input = @"<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>".Split(Environment.NewLine);
+
+            Day12.Parse(input).Should().BeEquivalentTo(new[]
+            {
+                new Vector(-1, 0, 2),
+                new Vector(2, -10, -7),
+                new Vector(4, -8, 8),
+                new Vector(3, 5, -1),
+            });
+        }
+
+        [Fact]
+        public void GetNewVelocityTest()
+        {
+            var current = new Vector(0,0,0);
+            var other = new Vector(3,0,-2);
+            Day12.GetForce(current, other).Should()
+                .BeEquivalentTo(new Vector(1, 0, -1));
+        }
+
+        [Fact]
+        public void RunTest()
+        {
+            var input = @"
+<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>".Split(Environment.NewLine).Where(s=>!string.IsNullOrEmpty(s)).ToArray();
+
+            var state = Day12.LoadInitialState(input);
+            var result = (IEnumerable<(Vector p, Vector v)>) Day12.Step(state).ToList();
+
+            var expected = @"
+<x= 2, y=-1, z= 1>
+<x= 3, y=-7, z=-4>
+<x= 1, y=-7, z= 5>
+<x= 2, y= 2, z= 0>".Split(Environment.NewLine).Where(s=>!string.IsNullOrEmpty(s)).ToArray();
+            var exp = Day12.Parse(expected);
+
+            result.Select(r=>r.p) .Should().BeEquivalentTo(exp);
+
+        }
+
+        [Fact]
+        public void EnergyTest()
+        {
+            var input = @"
+<x=-8, y=-10, z=0>
+<x=5, y=5, z=10>
+<x=2, y=-7, z=3>
+<x=9, y=-8, z=-3>".Split(Environment.NewLine).Where(s=>!string.IsNullOrEmpty(s)).ToArray();
+            Day12.GetEnergyAfterSteps(input, 100).Should().Be(1940);
+
+        }
+    }
+}
