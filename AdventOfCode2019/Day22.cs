fb3e84e79015e3020e454260dad923431a63d716
FrankBakkerNl <frank.bakker@avivasolutions.nl>
2019-12-30 01:22:02 +0100

    D22:P2

---
+++ AdventOfCode2019/Day22.cs
@@ -0,0 +1,168 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2019
+{
+    public class Day22
+    {
+        public static BigInteger GetAnswer1(string[] instructions) => FindNewPosition(instructions, 10007, 2019); 
+        // 4485
+
+        public static BigInteger FullDeckSize = 119_315_717_514_047;
+        public static BigInteger LoopCount = 101_741_582_076_661;
+
+        public static BigInteger GetAnswer2(string[] instructions) 
+            => FindOriginalPosition(instructions, FullDeckSize, 2020, LoopCount);
+        // 91967327971097
+
+        private static readonly (BigInteger a, BigInteger b) NoOpShuffle = (1, 0);
+
+        public static BigInteger FindNewPosition(string[] instructions, BigInteger deckSize, BigInteger initialPosition)
+        {
+            var parsed = instructions.Select(ParseInstruction);
+            var (a, b) = parsed.Select(p => GetABForOperation(p, deckSize))
+                .Aggregate(NoOpShuffle, MergeFactors);
+
+            return ModuloShift(initialPosition * a + b, deckSize);
+        }
+
+        public enum Operation { DealIntoNewStack, Cut, DealWithIncrement }
+
+         public static (Operation, long) ParseInstruction(string line)
+         {
+             if (line == "deal into new stack")
+                 return (Operation.DealIntoNewStack, 0);
+             var match = Regex.Match(line, "(?<i>[a-z ]*) (?<n>-?[0-9]+)");
+             var instruction = match.Groups["i"].Value switch
+             {
+                 "deal with increment" => Operation.DealWithIncrement,
+                 "cut" => Operation.Cut
+             };
+             return (instruction, long.Parse(match.Groups["n"].Value));
+         }
+
+         /// <summary>
+         /// Each shuffle can be expressed as a function y = ax+b % deckSize,
+         /// here we find just the A and B for an operation
+         /// </summary>
+         public static (BigInteger a, BigInteger b) GetABForOperation( 
+             (Operation type, BigInteger p) instruction, BigInteger deckSize)
+         {
+             var values = instruction.type switch
+             {
+                 Operation.DealIntoNewStack =>  (a: -BigInteger.One,       b: deckSize - 1L), // y = -x + ds - 1
+                 Operation.Cut =>               (a: BigInteger.One,        b: deckSize - instruction.p), // y = x + ds - p
+                 Operation.DealWithIncrement => (a: instruction.p,         b: 0L) // y = p * x
+             };
+             return ModuloShift(values, deckSize);
+         }
+
+         /// <summary>
+         /// Combines the A and B components of two y=ax+b functions into the A and B one new combined function
+         /// Such that the resulting function will be F2(F1))
+         /// </summary>
+         public static (BigInteger a, BigInteger b) MergeFactors((BigInteger a, BigInteger b) f1, (BigInteger a, BigInteger b) f2) =>
+             (a: f2.a * f1.a, 
+              b: f2.a * f1.b + f2.b);
+         /* we add two functions together like this
+          * F1(x) = a1 * x + b1
+          * F2(x) = a2 * x + b2
+          * Fr(x) = F2   (F1(x)      )
+          * Fr(x) = a2 * (a1 * x + b1) + b2
+          * Fr(x) = a2 * a1 * x + a2 * b1 + b2
+          *         |  a  | * x + |    b     |
+          */
+
+        public static BigInteger FindOriginalPosition(string[] instructions, BigInteger deckSize, BigInteger finalPosition, BigInteger loopCount)
+        {
+            var parsed = instructions.Select(ParseInstruction);
+            var factors = parsed.Select(p => GetABForOperation(p, deckSize))
+                .Aggregate(NoOpShuffle, MergeFactors);
+
+            var (a, b) = MultiplyFactors(factors, loopCount, deckSize);
+
+            return ReverseLookup(finalPosition, a, b, deckSize);
+        }
+
+        public static (BigInteger, BigInteger) MultiplyFactors((BigInteger a, BigInteger b) initialFactors, BigInteger loopCount, BigInteger deckSize)
+        {
+            // We need to apply F(F(F(x))) MANY times (101_741_582_076_661)
+            // Instead of adding the function one by one we keep on duplicating it
+            // so we get the results for all the squares 1, 2, 4, 8, 16
+            // then we add up all required squares to the result
+
+            var current = initialFactors;
+            var result = (a: BigInteger.One, b: BigInteger.Zero);
+
+            // loop fast by duplicating each time so we loop just around 47 times 
+            for (BigInteger i = 1; i <= loopCount; i *= 2)
+            {
+                // add the current to the result if the loopCount contains the bit for this iteration
+                if ((loopCount & i) != 0)
+                {
+                    result = ModuloShift(MergeFactors(current, result), deckSize);
+                }
+                // Merge the current with itself so we get the values for the next square number
+                current = ModuloShift(MergeFactors(current, current), deckSize);
+            }
+
+            return result;
+        }
+
+        public static BigInteger ReverseLookup(BigInteger finalPosition, BigInteger a, BigInteger b, BigInteger deckSize)
+        {
+            // We need the inverse of Y = (A * X + B) % deckSize
+
+            // subtract b first and shift back
+            var offSetFinalPosition = ModuloShift(finalPosition - b, deckSize);
+
+            // for the reverse lookup we need the Multiplicative Inverse of A
+            var inverseA = MultiplicativeInverse(a, deckSize);
+            var initialPosition = ModuloShift(offSetFinalPosition * inverseA, deckSize);
+
+            // double check the result
+            Debug.Assert( ModuloShift(initialPosition * a + b, deckSize) == finalPosition);
+            return initialPosition;
+        }
+
+        public static BigInteger MultiplicativeInverse(BigInteger x, BigInteger modulus)
+        {
+            // Thanks to Eric https://ericlippert.com/2013/11/14/a-practical-use-of-multiplicative-inverses/
+            // for explaining Multiplicative Inverse
+            return ExtendedEuclideanDivision(x, modulus).Item1 % modulus;
+        }
+
+        public static (BigInteger, BigInteger)
+            ExtendedEuclideanDivision(BigInteger a, BigInteger b)
+        {
+            if (a < 0)
+            {
+                var (s, t) = ExtendedEuclideanDivision(-a, b);
+                return (-s, t);
+            }
+            if (b < 0)
+            {
+                var (s, t) = ExtendedEuclideanDivision(a, -b);
+                return (s, -t);
+            }
+            if (b == 0)
+                return (1, 0);
+            else
+            {
+                var (s, t) = ExtendedEuclideanDivision(b, a % b);
+                return (t, s - a / b * t);
+            }
+        }
+
+        public static BigInteger ModuloShift(BigInteger pos, BigInteger deckSize)
+        {
+            pos %= deckSize;
+            return pos >= 0 ? pos : pos + deckSize;
+        }
+
+        static (BigInteger a, BigInteger b) ModuloShift((BigInteger a, BigInteger b) input, BigInteger deckSize) =>
+            (ModuloShift(input.a, deckSize), ModuloShift(input.b, deckSize));
+    }
+}
---
+++ AdventOfCodeTest2019Test/Day22Test.cs
@@ -0,0 +1,143 @@
+﻿using System.Numerics;
+using AdventOfCode2019;
+using FluentAssertions;
+using Xunit;
+using static AdventOfCode2019.Day22;
+
+namespace AdventOfCodeTest2019Test
+{
+    public class Day22Test
+    {
+        [Fact]
+        public void ParseInstructionTest()
+        {
+            ParseInstruction("deal into new stack").Item1
+                .Should().Be(Operation.DealIntoNewStack);
+            ParseInstruction("cut -2")
+                .Should().Be((Operation.Cut, -2));
+            ParseInstruction("deal with increment 7")
+                .Should().Be((Operation.DealWithIncrement, 7));
+        }
+
+        [Fact]
+        public void Shuffle1Test()
+        {
+            var input = new[]
+            {
+                "deal with increment 7",
+                "deal into new stack",
+                "deal into new stack",
+            };
+            ShuffleDeck(10, input).Should().Be("0 3 6 9 2 5 8 1 4 7");
+            ReverseShuffleDeck(10, input).Should().Be("0 3 6 9 2 5 8 1 4 7");
+        }
+
+        [Fact]
+        public void Shuffle2Test()
+        {
+            var input = new[]
+            {
+                "cut 6",
+                "deal with increment 7",
+                "deal into new stack", 
+            };
+            ShuffleDeck(10, input).Should().Be("3 0 7 4 1 8 5 2 9 6");
+            ReverseShuffleDeck(10, input).Should().Be("3 0 7 4 1 8 5 2 9 6");
+        }
+        [Fact]
+        public void Shuffle3Test()
+        {
+            var input = new[]
+            {
+                "deal with increment 7",
+                "deal with increment 9",
+                "cut -2",
+            }; 
+            ShuffleDeck(10, input).Should().Be("6 3 0 7 4 1 8 5 2 9");
+            ReverseShuffleDeck(10, input).Should().Be("6 3 0 7 4 1 8 5 2 9");
+        }
+
+        [Fact]
+        public void Shuffle4Test()
+        {
+            var input = new[]
+            {
+                "deal into new stack",
+                "cut -2",
+                "deal with increment 7",
+                "cut 8",
+                "cut -4",
+                "deal with increment 7",
+                "cut 3",
+                "deal with increment 9",
+                "deal with increment 3",
+                "cut -1",
+            };
+            ShuffleDeck(10, input).Should().Be("9 2 5 8 1 4 7 0 3 6");
+            ReverseShuffleDeck(10, input).Should().Be("9 2 5 8 1 4 7 0 3 6");
+        }
+
+        [Fact]
+        public void LoopTest()
+        {
+            var deckSize = (BigInteger) 119_315_717_514_047;
+
+            var input = new[]
+            {
+                "deal into new stack",
+                "cut -2",
+                "deal with increment 7",
+                "cut 8",
+                "cut -4",
+                "deal with increment 7",
+                "cut 3",
+                "deal with increment 9",
+                "deal with increment 3",
+                "cut -1",
+            };
+
+            var loopCount = 10211;
+            var initialPosition = 2019;
+            BigInteger lastPos = initialPosition;
+            for (int i = 0; i < loopCount; i++)
+            {
+                lastPos = FindNewPosition(input, deckSize, lastPos);
+            }
+
+            FindOriginalPosition(input, deckSize, lastPos, loopCount).Should().Be(initialPosition);
+        }
+
+        [Theory]
+        [InlineData("deal into new stack",   "9 8 7 6 5 4 3 2 1 0")]
+        [InlineData("cut 3",                 "3 4 5 6 7 8 9 0 1 2")]
+        [InlineData("cut -4",                "6 7 8 9 0 1 2 3 4 5")]
+        [InlineData("deal with increment 3", "0 7 4 1 8 5 2 9 6 3")]
+        public void BasicShuffles(string instruction, string result)
+        {
+            ShuffleDeck(10, instruction).Should().Be(result);
+            ReverseShuffleDeck(10, instruction).Should().Be(result);
+        }
+       
+        private string ShuffleDeck(int deckSize, params string[] instructions)
+        {
+            var newDeck = new long[deckSize];
+            for (int i = 0; i < deckSize; i++)
+            {
+                var position = Day22.FindNewPosition(instructions, deckSize, i);
+                newDeck[(int)position] = i;
+            }
+            return string.Join(" ", newDeck);
+        }
+
+        private string ReverseShuffleDeck(int deckSize, params string[] instructions)
+        {
+            var newDeck = new long[deckSize];
+            for (int i = 0; i < deckSize; i++)
+            {
+                var original = (int)Day22.FindOriginalPosition(instructions, deckSize, i, 1);
+                newDeck[i] = original;
+            }
+            return string.Join(" ", newDeck);
+        }
+    }
+}
