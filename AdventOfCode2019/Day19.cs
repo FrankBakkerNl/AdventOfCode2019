44b8f3c1b522ac44e89a5ce36f4e07103c7f2f30
FrankBakkerNl <frank.bakker@avivasolutions.nl>
2020-01-08 12:17:06 +0100

    Day19:P1

---
+++ AdventOfCode2019/Day19.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AdventOfCode2019.VM;
+using static System.Linq.Enumerable;
+
+namespace AdventOfCode2019
+{
+    public class Day19
+    {
+        public int GetAnswer1(string input)
+        {
+            bool IsAffected(int x, int y)
+            {
+                var cpu = new IntCodeComputer(input);
+                cpu.Run(x,y);
+                return cpu.ReadOutput() == 1;
+            }
+
+            return Range(0, 50).SelectMany(i => Range(0, 50).Select(j => (i, j))).Count(s => IsAffected(s.i, s.j));
+        }
+    }
+}
