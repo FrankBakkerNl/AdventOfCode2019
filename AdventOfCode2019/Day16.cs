364faaf059635e38a4f0361e8d8df489ffabd629
FrankBakkerNl <frank.bakker@avivasolutions.nl>
2020-01-24 18:02:59 +0100

    Day16:P2

--- AdventOfCode2019/Day16.cs
+++ AdventOfCode2019/Day16.cs
@@ -1,18 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using AdventOfCode2019.VM;
+﻿using System.Linq;
 using static System.Math;
 
 namespace AdventOfCode2019
 {
-    [Test]
     public class Day16
     {
 
         [Result(63483758)]
-        public static string GetAnswer1(string input) => MergeOutput( GetValues(ParseInputDigits(input), 100, 0, 8));
+        public static string GetAnswer1(string input) => MergeOutput(GetValues(ParseInputDigits(input), 100, 0, 8));
+
+        [Result(96099551)]
+        public static string GetAnswer2(string input)
+        {
+            var inputDigits = ParseInputDigits(input);
+
+            // repeat 10.000 times
+            var fullInput = Enumerable.Repeat(inputDigits, 10_000).SelectMany(i=>i).ToArray();
+            var messageOffset = int.Parse(input.Substring(0, 7));
+
+            var values = SimulatePhasesBottom(fullInput, 100).Skip(messageOffset).Take(8).ToArray();
+            return MergeOutput(values);
+        }
+
 
         static int[] GetValues(int[] input, int phases, int start, int outputLength)
         {
@@ -26,11 +35,8 @@ namespace AdventOfCode2019
             var output = new int[input.Length];
             for (int iteration = 0; iteration < iterations; iteration++)
             {
-                Console.WriteLine(iteration);
                 GetNextPhase(input, output);
-                var temp = output;
-                output = input;
-                input = temp;
+                Swap(ref input, ref output);
             }
 
             return input;
@@ -40,7 +46,6 @@ namespace AdventOfCode2019
         {
             for (int i = 0; i < input.Length; i++)
             {
-
                 var sum = 0;
 
                 var repeatCount = i + 1;
@@ -49,22 +54,60 @@ namespace AdventOfCode2019
 
                 for (int j = startSkipCount; j < input.Length; j+=patternLength)
                 {
-                    for (int repeat = 0; repeat < repeatCount && j+repeat < input.Length; repeat++)
+                    var end = Min(j + repeatCount, input.Length);
+                    for (int k = j; k< end; k++)
                     {
-                        sum += input[j+repeat];
+                        sum += input[k];
                     }
                 }
                 startSkipCount = (repeatCount * 3)-1;
 
                 for (int j = startSkipCount; j < input.Length; j+=patternLength)
                 {
-                    for (int repeat = 0; repeat < repeatCount && j+repeat < input.Length; repeat++)
+                    var end = Min(j + repeatCount, input.Length);
+                    for (int k = j; k< end; k++)
                     {
-                        sum -= input[j+repeat];
+                        sum -= input[k];
                     }
                 }
                 output[i] = Abs(sum) % 10;
             }
+        }
+
+
+        public static int[] SimulatePhasesBottom(int[] input, int iterations)
+        {
+            // create one extra buffer and keep swapping them
+            var output = new int[input.Length];
+            for (int iteration = 0; iteration < iterations; iteration++)
+            {
+                GetNextPhaseBottom(input, output);
+                Swap(ref input, ref output);
+            }
+
+            return input;
+        }
+
+
+        public static void GetNextPhaseBottom(int[] input, int[] output)
+        {
+            var accumulator = 0;
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                for (int j = i; j >= i; j--)
+                {
+                    accumulator += input[j];
+                }
+
+                output[i] = accumulator %10;
+            }
+        }
+
+        static void Swap<T>(ref T x, ref T y)
+        {
+            T t = x;
+            x = y;
+            y = t;
         }
 
         private static int[] ParseInputDigits(string input) => input.Select(i => i - '0').ToArray();
